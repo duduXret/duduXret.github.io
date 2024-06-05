50c6698b747a9db33b74a5617d877a6370135789
duduXret <126572351+duduXret@users.noreply.github.com>
2024-06-05 12:18:36 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align='center' > Portifólio web 🖼️
+
+<h1/>
+
+ <div>
+ 
+<h5>
+Projeto pensado para que tenha uma maior visibilidade sobre meus projetos realizados recentemente.
+Também compartilhando informações para contato, junto ao link de minhas redes sociais.
+<h5/>
+
+<div/>
+
+##
+
+Utilizei as seguintes Linguagens:
+
+ HTML <br/>
+ CSS <br/>
+ JavaScript <br/>
+ 
+## 
+
+
+
+
+
+https://user-images.githubusercontent.com/91097315/214154776-3e3202df-6dda-4e87-9ef5-79639bfc0611.mp4
+
+
---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+        // *************** MENU MOBILE **********************
+        class MobileNavbar {
+            constructor(mobileMenu, navList, navLinks) {
+            this.mobileMenu = document.querySelector(mobileMenu);
+            this.navList = document.querySelector(navList);
+            this.navLinks = document.querySelectorAll(navLinks);
+            this.activeClass = "active";
+        
+            this.handleClick = this.handleClick.bind(this);
+            }
+        
+            animateLinks() {
+            this.navLinks.forEach((link, index) => {
+                link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.3s ease forwards ${
+                    index / 7 + 0.3
+                    }s`);
+            });
+            }
+        
+            handleClick() {
+            this.navList.classList.toggle(this.activeClass);
+            this.mobileMenu.classList.toggle(this.activeClass);
+            this.animateLinks();
+            }
+        
+            addClickEvent() {
+            this.mobileMenu.addEventListener("click", this.handleClick);
+            }
+        
+            init() {
+            if (this.mobileMenu) {
+                this.addClickEvent();
+            }
+            return this;
+            }
+        }
+        
+        const mobileNavbar = new MobileNavbar(
+            ".mobile-menu",
+            ".menu",
+            ".menu li",
+        );
+        mobileNavbar.init();  
+        
+        
+        // *************** BTN VOLTAR PARA CIMA **********************
+
+        document.addEventListener("DOMContentLoaded", function () {
+          const verMaisBtn = document.getElementById("verMais");
+          const showMoreDiv = document.querySelector(".showMore");
+
+          verMaisBtn.addEventListener("click", function () {
+            showMoreDiv.style.display = "flex";
+            verMaisBtn.style.display = "none";
+          });
+        });
